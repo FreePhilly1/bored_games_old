d3191bea1b00b60289fb20a38a950ffcc7e199de
Phillip Kim <pkim90@gatech.edu>
2022-09-04 23:25:53 -0400

    finished challenge feature

--- src/components/PlayerCards.js
+++ src/components/PlayerCards.js
@@ -9,7 +9,7 @@ export default function PlayerCards(props) {
                 Object.keys(playerStates).length > 0 && 
                 Object.keys(playerStates).map((key) => {
                     return (
-                        <div style={{ width: '200px', height: '100px', margin: '20px', borderRadius: '20px', backgroundColor: "red"}}>
+                        <div style={{ width: '200px', height: '100px', margin: '20px', borderRadius: '20px', backgroundColor: "yellow"}}>
                             <p>{key}</p>
                             <p>Cards: {playerStates[key].cards.toString()}</p>
                             <p>Coins: {playerStates[key].coins}</p>
